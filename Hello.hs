e0f8d0d755cd0eae71f748d537956168bab1bc78
logeast <logeast@outlook.com>
2022-06-28 14:37:08 +0800

    feat: safer html construction with types

--- Hello.hs
+++ Hello.hs
@@ -1,3 +1,5 @@
+-- Inspared by https://lhbg-book.link/
+
 main :: IO ()
 main = putStrLn (render myhtml)
 
@@ -13,12 +15,16 @@ myhtml =
         )
     )
 
+-- A complete Html document.
 newtype Html = Html String
 
+-- A type for html structures, such as head, h1, p, etc.
 newtype Structure = Structure String
 
+-- Title is just a type name alias.
 type Title = String
 
+-- Combine html tags.
 html_ :: Title -> Structure -> Html
 html_ title content =
   Html
@@ -29,16 +35,31 @@ html_ title content =
         )
     )
 
+-- Because,
+-- Structure :: String -> Structure
+-- el "p" :: String -> String
+-- So,
+-- a ~ String
+-- b ~ String
+-- c ~ Structure
+-- Because,
+-- (.) :: (b -> c) -> (a -> b) -> (a -> c)
+-- So,
+-- (.) :: (String -> Structure) -> (String -> String) -> (String -> Structure)
+-- So,
+-- Structure . el "p" :: String -> Structure
 p_ :: String -> Structure
-p_ = Structure . el "title"
+p_ = Structure . el "p"
 
 h1_ :: String -> Structure
 h1_ = Structure . el "h1"
 
+-- A function that take a tag and content, and wraps the content with the tag.
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+-- Replace <> operator. Take two Structures and return a third Structure.
 append_ :: Structure -> Structure -> Structure
 append_ (Structure a) (Structure b) = Structure (a <> b)
 
@@ -47,6 +68,7 @@ getStructureString content =
   case content of
     Structure str -> str
 
+-- Converting back Html to String. So we can display it in our browser.
 render :: Html -> String
 render html =
   case html of
